daab910e6d9fed524e981e92fb81c0364940522f
TheKhoshhal <thekhoshhal@gmail.com>
2024-06-25 19:05:31 +0330

    configured helix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,14 +3,15 @@ if status is-interactive
 end
 
 ### alias ###
-alias vim "nvim"
-alias vi "/usr/bin/vim"
+alias vim nvim
+alias vi /usr/bin/vim
+alias hx helix
 alias sourcefish "source ~/.config/fish/config.fish"
 
 # eza
 alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
+alias la 'eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll 'eza -l --color=always --group-directories-first --icons' # long format
 alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
 alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 
