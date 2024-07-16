aeb0137b92d11e696cec45f1f9500c4b9cb7f96b
TheKhoshhal <thekhoshhal@gmail.com>
2024-07-16 21:57:21 +0330

    added new rofi theme

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,5 @@
+/home/thekhoshhal/Music/Cartoon - On & On (feat. Daniel Levi) [NCS Release].mp3
+/home/thekhoshhal/Music/Culture Code - Make Me Move (feat. Karra) [NCS Release].mp3
+/home/thekhoshhal/Music/Janji - Heroes Tonight (feat. Johnning) [NCS Release].mp3
+/home/thekhoshhal/Music/N3WPORT - Power (feat. braev) [NCS Release].mp3
+/home/thekhoshhal/Music/NEFFEX - Grateful [Copyright Free].mp3
